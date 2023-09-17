3d603e82bdcf31a1e1f58387785e9f0dd7433f1a
szigetizoli <szigetizoli@mailbox.hu>
2023-09-17 23:01:05 +0200

    HTML canvas, kepek, kep megjelenitese a canvason

--- minesweeper.js
+++ minesweeper.js
@@ -1,1 +1,4 @@
-console.log("hello világ");
+var image = document.getElementById('hidden');
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+ctx.drawImage(image, 0, 0); // A kép bal felső sarka (0,0) koordinátára kerülß
